28759798d1162e34c3be7556ecc751921c4aad8e
Pranav Puri <pranavpuri.p@hotmail.com>
2017-01-08 17:38:53 +0530

    styling done

---
+++ src/static/static_dirs/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    // $('body').hide()
+    // site preloader -- also uncomment the div in the header and the css style for #preloader
+    $(window).load(function() {
+        $('.progress').fadeOut('slow');
+        // $('body').show();
+    });
+
+});
